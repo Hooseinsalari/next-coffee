4dfa75465dec18eba756e12a8a129aa5be690775
Hossein Salari <hossein.salari002@gmail.com>
2023-12-20 15:08:05 +0330

    feat(Navbar): implement navbar component with styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
+// components
+import Navbar from "@/components/modules/Navbar/Navbar";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
