e633508d72511ce1b43f66c52175012b67d84538
Hossein Salari <hossein.salari002@gmail.com>
2023-12-22 23:38:05 +0330

    feat(Menu): implement menu section and product card

--- components/modules/ProductCard/ProductCard.js
+++ components/modules/ProductCard/ProductCard.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
-const ProductCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProductCard = ({ title, price, desc, img: imgSrc }) => {
+  return (
+    <div className="row align-items-center mb-5">
+      <div className="col-4 col-sm-3">
+        <img
+          className="w-100 rounded-circle mb-3 mb-sm-0"
+          src={imgSrc}
+          alt=""
+        />
+        <h5 className="menu-price">${price}</h5>
+      </div>
+      <div className="col-8 col-sm-9">
+        <h4>{title}</h4>
+        <p className="m-0">{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import About from "@/components/modules/About/About";
+import Menu from "@/components/templates/Index/Menu";
 import Offer from "@/components/templates/Index/Offer";
 import Services from "@/components/templates/Index/Services";
 import Slider from "@/components/templates/Index/Slider";
 
-export default function Home() {
+export default function Home({ menuData }) {
   return (
     <>
       <Slider />
       <About />
       <Services />
       <Offer />
+      <Menu menuData={menuData} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const menuResponse = await fetch("http://localhost:4000/menu");
+  const menuData = await menuResponse.json();
+
+  return {
+    props: {
+      menuData,
+    },
+    revalidate: 60 * 60 * 12,
+  };
 }
