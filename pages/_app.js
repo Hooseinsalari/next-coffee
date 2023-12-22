d88308041a404a9624ae2e7b9992ca2802c3bc70
Hossein Salari <hossein.salari002@gmail.com>
2023-12-23 00:14:00 +0330

    feat(Footer): implement footer section

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,14 @@ config.autoAddCss = false;
 
 // components
 import Navbar from "@/components/modules/Navbar/Navbar";
+import Footer from "@/components/modules/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
