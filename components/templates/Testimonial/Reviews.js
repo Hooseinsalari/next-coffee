62c6689645f880a5eb0473bf1ba37900680837eb
Hossein Salari <hossein.salari002@gmail.com>
2024-01-01 22:45:39 +0330

    feat(Testimonial): implement testimonial page

--- components/templates/Testimonial/Reviews.js
+++ components/templates/Testimonial/Reviews.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import CommentCard from "@/components/modules/CommentCard/CommentCard";
+import React from "react";
 
-const Reviews = () => {
-    return (
-        <div>
-            
+const Reviews = ({ comments }) => {
+  return (
+    <div className="container-fluid py-5">
+      <div className="container">
+        <div className="section-title">
+          <h4
+            className="text-primary text-uppercase"
+            style={{ letterSpacing: "5px" }}
+          >
+            Testimonial
+          </h4>
+          <h1 className="display-4">Our Clients Say</h1>
         </div>
-    );
+        <div className="owl-carousel testimonial-carousel">
+          {comments.map((item) => (
+            <CommentCard {...item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,9 +1,25 @@
-function Testimonial() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+import PageHeader from "@/components/modules/PageHeader/PageHeader";
+import Reviews from "@/components/templates/Testimonial/Reviews";
+
+function Testimonial({ comments }) {
+  return (
+    <>
+      <PageHeader title="Testimonial" />
+      <Reviews comments={comments} />
+    </>
+  );
 }
 
 export default Testimonial;
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:4000/comments");
+  const comments = await response.json();
+
+  return {
+    props: {
+      comments,
+    },
+    revalidate: 12 * 60 * 60,
+  };
+}
